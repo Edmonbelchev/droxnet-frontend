6cca346d19319c4c3428afd7b6d617a3477d51d2
Edmon Belchev <edmon.cekov@gmail.com>
2024-03-26 22:14:04 +0200

    Removed unknow route

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -39,7 +39,7 @@
         />
         <NuxtLink to="/login" class="text-blue-400">Login</NuxtLink>
         <NuxtLink
-          to="/register"
+          to="/"
           class="bg-[--primary-color] text-white px-4 py-2 rounded"
           >Join now</NuxtLink
         >

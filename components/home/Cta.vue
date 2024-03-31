9eb02ec5885e269adc6acce23bc476298c02e132
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2024-03-31 18:43:21 +0300

    created new component

---
+++ components/home/Cta.vue
@@ -0,0 +1,29 @@
+<template>
+
+    <div class="flex justify-center relative overflow-hidden">
+        <div class="w-1/4 bg-gray-800 absolute left-0 h-full">
+            <img src="https://amentotech.com/htmls/worktern/images/bg/img-01.jpg" alt="" class="opacity-80 w-full h-full">
+        </div>
+
+      <div class="flex w-3/5 py-32 bg-white z-10">
+        <div class="row text-center content-center px-8">
+            <h2 class="text-3xl font-bold text-slate-900 mb-5">Start As Company</h2>
+            <p class="text-gray-500 text-base mb-8 leading-7">Consectetur adipisicing elit sed dotem eiusmod tempor incune utnaem labore etdolore maigna aliqua enim
+                poskina ilukita ylokem lokateise ination voluptate velit esse cillum.</p>
+            <a href="#" class="primary-button">Join now</a>
+        </div>
+
+        <div class="row text-center content-center px-8">
+            <h2 class="text-3xl font-bold text-slate-900 mb-5">Start As Freelancer</h2>
+            <p class="text-gray-500 text-base mb-8 leading-7">Consectetur adipisicing elit sed dotem eiusmod tempor incune utnaem labore etdolore maigna aliqua enim
+                poskina ilukita ylokem lokateise ination voluptate velit esse cillum.</p>
+            <a href="#" class="primary-button">Join now</a>
+        </div>
+      </div>
+
+        <div class="right-0 w-1/4 bg-gray-800 absolute right-0 h-full">
+            <img src="https://amentotech.com/htmls/worktern/images/bg/img-02.jpg" alt="" class="opacity-80 w-full h-full">
+        </div>
+    </div>
+
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,95 +1,91 @@
 <script setup lang="ts">
 const categories = [
-  {
-    image: "https://droxnet.com/images/categories/img-01.png",
-    title: "Web Developer",
-    description:
-      "Consectetur adipisicing elitaed eiusmod tempor incididuatna labore et dolore magna.",
-    route: "/",
-  },
-  {
-    image: "https://droxnet.com/images/categories/img-08.png",
-    title: "Software Developer",
-    description:
-      "Consectetur adipisicing elitaed eiusmod tempor incididuatna labore et dolore magna.",
-    route: "/",
-  },
-  {
-    image: "https://droxnet.com/images/categories/img-02.png",
-    title: "Mobile Apps",
-    description:
-      "Consectetur adipisicing elitaed eiusmod tempor incididuatna labore et dolore magna.",
-    route: "/",
-  },
-  {
-    image: "https://droxnet.com/images/categories/img-03.png",
-    title: "Data Entry",
-    description:
-      "Consectetur adipisicing elitaed eiusmod tempor incididuatna labore et dolore magna.",
-    route: "/",
-  },
-  {
-    image: "https://droxnet.com/images/categories/img-04.png",
-    title: "Design",
-    description:
-      "Consectetur adipisicing elitaed eiusmod tempor incididuatna labore et dolore magna.",
-    route: "/",
-  },
-  {
-    image: "https://droxnet.com/images/categories/img-05.png",
-    title: "Seo",
-    description:
-      "Consectetur adipisicing elitaed eiusmod tempor incididuatna labore et dolore magna.",
-    route: "/",
-  },
-  {
-    image: "https://droxnet.com/images/categories/img-06.png",
-    title: "Marketing",
-    description:
-      "Consectetur adipisicing elitaed eiusmod tempor incididuatna labore et dolore magna.",
-    route: "/",
-  },
-  {
-    image: "https://droxnet.com/images/categories/img-07.png",
-    title: "Copywriter",
-    description:
-      "Consectetur adipisicing elitaed eiusmod tempor incididuatna labore et dolore magna.",
-    route: "/",
-  },
+    {
+        image: "https://droxnet.com/images/categories/img-01.png",
+        title: "Web Developer",
+        description:
+            "Consectetur adipisicing elitaed eiusmod tempor incididuatna labore et dolore magna.",
+        route: "/",
+    },
+    {
+        image: "https://droxnet.com/images/categories/img-08.png",
+        title: "Software Developer",
+        description:
+            "Consectetur adipisicing elitaed eiusmod tempor incididuatna labore et dolore magna.",
+        route: "/",
+    },
+    {
+        image: "https://droxnet.com/images/categories/img-02.png",
+        title: "Mobile Apps",
+        description:
+            "Consectetur adipisicing elitaed eiusmod tempor incididuatna labore et dolore magna.",
+        route: "/",
+    },
+    {
+        image: "https://droxnet.com/images/categories/img-03.png",
+        title: "Data Entry",
+        description:
+            "Consectetur adipisicing elitaed eiusmod tempor incididuatna labore et dolore magna.",
+        route: "/",
+    },
+    {
+        image: "https://droxnet.com/images/categories/img-04.png",
+        title: "Design",
+        description:
+            "Consectetur adipisicing elitaed eiusmod tempor incididuatna labore et dolore magna.",
+        route: "/",
+    },
+    {
+        image: "https://droxnet.com/images/categories/img-05.png",
+        title: "Seo",
+        description:
+            "Consectetur adipisicing elitaed eiusmod tempor incididuatna labore et dolore magna.",
+        route: "/",
+    },
+    {
+        image: "https://droxnet.com/images/categories/img-06.png",
+        title: "Marketing",
+        description:
+            "Consectetur adipisicing elitaed eiusmod tempor incididuatna labore et dolore magna.",
+        route: "/",
+    },
+    {
+        image: "https://droxnet.com/images/categories/img-07.png",
+        title: "Copywriter",
+        description:
+            "Consectetur adipisicing elitaed eiusmod tempor incididuatna labore et dolore magna.",
+        route: "/",
+    },
 ];
 </script>
 
 
 <template>
-  <NuxtLayout name="default">
-    <main>
-      <div class="relative z-0">
-        <HomeBanner />
-      </div>
+    <NuxtLayout name="default">
+        <main>
+            <div class="relative z-0">
+                <HomeBanner />
+            </div>
 
-      <!-- grid grid-cols-4 -->
-      <div class="py-20 flex flex-col justify-center items-center">
-        <div class="flex flex-col gap-2 mb-20 text-center">
-          <h2 class="text-4xl text-gray-800 font-bold">Explore Categories</h2>
-          <span
-            class="text-lg relative after:content-[''] after:absolute after:-bottom-4 after:left-0 after:right-0 after:mx-auto after:w-40 after:h-1 after:bg-[--primary-color] after:rounded-lg"
-            >Professional by categories</span
-          >
-        </div>
+            <!-- grid grid-cols-4 -->
+            <div class="py-20 flex flex-col justify-center items-center">
+                <div class="flex flex-col gap-2 mb-20 text-center">
+                    <h2 class="text-4xl text-gray-800 font-bold">Explore Categories</h2>
+                    <span
+                        class="text-lg relative after:content-[''] after:absolute after:-bottom-4 after:left-0 after:right-0 after:mx-auto after:w-40 after:h-1 after:bg-[--primary-color] after:rounded-lg">Professional
+                        by categories</span>
+                </div>
 
-        <div class="grid grid-cols-4 gap-8 mb-12">
-          <CategoryItem
-            :image="category.image"
-            :title="category.title"
-            :description="category.description"
-            :route="category.route"
-            v-for="(category, index) in categories"
-            :key="index"
-          />
-        </div>
+                <div class="grid grid-cols-4 gap-8 mb-12">
+                    <CategoryItem :image="category.image" :title="category.title" :description="category.description"
+                        :route="category.route" v-for="(category, index) in categories" :key="index" />
+                </div>
 
-        <NuxtLink to="/" class="primary-button">View All Categories</NuxtLink>
-      </div>
-    </main>
-  </NuxtLayout>
+                <NuxtLink to="/" class="primary-button">View All Categories</NuxtLink>
+            </div>
+
+                <HomeCta /> 
+
+        </main>
+    </NuxtLayout>
 </template>
